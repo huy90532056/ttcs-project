de21fc9071a64be00c2fb79b425ccea74df54a61
huy90532056 <huytruong90532056@gmail.com>
2025-04-21 20:52:58 +0700

    updating 2

--- ecommerce-web/src/main/java/com/shopee/ecommerce_web/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.shopee.ecommerce_web.repository;
-
-import com.shopee.ecommerce_web.entity.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AddressRepository extends JpaRepository<Address, String> {
-}
